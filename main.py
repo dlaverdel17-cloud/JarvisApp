0421020b5aa0c18f60f92dc20ff09a82d99e409b
dlaverdel17-cloud <dlaverde.l17@gmail.com>
2026-08-27 00:55:17 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from google import genai
+
+app = FastAPI(title="Jarvis AI Core")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# API KEY DE GEMINI
+GEMINI_API_KEY = " "
+
+# Cliente de Gemini
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+
+class QueryModel(BaseModel):
+    prompt: str
+
+
+@app.get("/")
+def home():
+    return {
+        "status": "JARVIS System Online"
+    }
+
+
+@app.post("/api/jarvis")
+async def process_jarvis(query: QueryModel):
+    try:
+
+        print("PREGUNTA RECIBIDA:", query.prompt)
+
+        response = client.models.generate_content(
+            model="gemini-3.6-flash",
+            contents=query.prompt
+        )
+
+        print("RESPUESTA:", response.text)
+
+        return {
+            "reply": response.text
+        }
+
+    except Exception as e:
+
+        print("ERROR DETALLADO:", repr(e))
+
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
